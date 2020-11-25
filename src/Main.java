7311ff3896dd9fdcd5041cf66b8b28d8a35813ee
kos-vkg <kvertenov@mail.ru>
2020-11-25 13:35:06 +0300

    error correction

--- src/Main.java
+++ src/Main.java
@@ -1,23 +1,15 @@
 public class Main {
     public static void main(String[] args) {
-  // провожу расчет с точностью рубля,
-  // т.к. сказано 1 руб за 100руб, а не 1%
-    long amount = 100; //начальная сумма в рублях
-    long refill  = 1111; // пополнение счета в рублях
-    long bonus;
-
-    if(refill > 1000){bonus = refill / 100; }
-    else {bonus  = 0;}
-    amount  += refill + bonus;
-
-         // вывод результата в руб.
-    System.out.print("бонус- ");
-    System.out.print( bonus);
-    System.out.println(" руб.");
-
-    System.out.print("итого на счету- ");
-    System.out.print(amount);
-    System.out.println(" руб.");
-
-     }
+        long amount = 100; //начальная сумма в рублях
+        long refill = 1111; // пополнение счета в рублях
+        long bonus;
+        if (refill > 1000) {
+            bonus = refill / 100;
+        } else {
+            bonus = 0;
+        }
+        amount += refill + bonus;
+        System.out.println("бонус- " + bonus + " руб.");
+        System.out.println("итого на счету- " + amount + " руб.");
+    }
 }
